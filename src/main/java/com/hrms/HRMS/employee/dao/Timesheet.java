506f040b7fc3bc4b77fa6945928e3e0ecda11124
AkashMynx <akash.raj@mynxsoftwares.com>
2025-03-26 22:50:33 +0530

    Formatted timesheet response local date

--- src/main/java/com/hrms/HRMS/employee/dao/Timesheet.java
+++ src/main/java/com/hrms/HRMS/employee/dao/Timesheet.java
@@ -3,6 +3,8 @@ package com.hrms.HRMS.employee.dao;
 import java.time.LocalDate;
 import java.time.LocalTime;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -23,8 +25,11 @@ public class Timesheet {
     @JoinColumn(name = "employeeId", nullable = false)
     private Employee employee;
 
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
     private LocalDate workDate;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
     private LocalTime clockIn;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
     private LocalTime clockOut;
     private double totalHours;
 }
