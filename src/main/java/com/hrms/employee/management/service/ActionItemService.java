1013c568ef4fc404054ea7f880ddea09482cef70
Manish.mehts <manish.mehts@mynxsoftwares.com>
2025-05-20 16:45:51 +0530

    missed annottation added

--- src/main/java/com/hrms/employee/management/service/ActionItemService.java
+++ src/main/java/com/hrms/employee/management/service/ActionItemService.java
@@ -8,8 +8,10 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
+import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+@Service
 public class ActionItemService {
 
     @Autowired
