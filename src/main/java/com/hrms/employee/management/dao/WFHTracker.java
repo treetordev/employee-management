18984b1c49121881d8b908dbd83bb2333bf45cc8
Manish.mehts <manish.mehts@mynxsoftwares.com>
2025-06-02 09:39:48 +0530

    table change

--- src/main/java/com/hrms/employee/management/dao/WFHTracker.java
+++ src/main/java/com/hrms/employee/management/dao/WFHTracker.java
@@ -5,14 +5,7 @@ import java.time.LocalDate;
 import com.fasterxml.jackson.annotation.JsonBackReference;
 import com.fasterxml.jackson.annotation.JsonFormat;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.PrePersist;
-import jakarta.persistence.PreUpdate;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -23,6 +16,7 @@ import lombok.NoArgsConstructor;
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
+@Table(name = "WfhTracker")
 public class WFHTracker {
 
     @Id
