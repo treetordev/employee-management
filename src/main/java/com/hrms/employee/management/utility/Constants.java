41e12aa6e8dfeb9a827fc2c19215961e55ba25a7
Manish.mehts <manish.mehts@mynxsoftwares.com>
2025-08-30 09:09:31 -0500

    fixed constant file

--- src/main/java/com/hrms/employee/management/utility/Constants.java
+++ src/main/java/com/hrms/employee/management/utility/Constants.java
@@ -1,7 +1,7 @@
 package com.hrms.employee.management.utility;
 
 public class Constants {
-    public static final Object ONBOARD_KEYCLOAK_USER = "/onboard-kc-user";
-    public static final String GET_ADMIN_TOKEN = "/keycloak-token" ;
-    public static final String GRANT_ADMIN_ACCESS ="/grant-super-admin-access" ;
+    public static final Object ONBOARD_KEYCLOAK_USER = "/iamcontroller/onboard-kc-user";
+    public static final String GET_ADMIN_TOKEN = "/iamcontroller/keycloak-token" ;
+    public static final String GRANT_ADMIN_ACCESS ="/iamcontrollrt/grant-super-admin-access" ;
 }
